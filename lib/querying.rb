4040f8f8fd6e2dd2ffed3b1d98782896081755fc
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-25 18:22:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
+  "SELECT series.title FROM series ORDER BY COUNT(species) WHERE species = 'human' DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
